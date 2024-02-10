3c55ad4f508a1ef0cdaae4d68118386b7bd896e0
mr-raashid <151937805+mr-raashid@users.noreply.github.com>
2024-02-10 10:07:57 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 16255;
+let accountEmail = "abcd@gmail.com";
+var accountPassword = "12345";
+accountCity = "Chennai";
+let accountState;
+
+// accountId = 16250; //not allowed to change the constant variable
+
+accountEmail = "1234@gmail.com";
+accountPassword = "54321";
+accountCity = "Vellore";
+
+/*
+Prefer not to use var because of issue
+in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict" // treat all JS code as newer version
+
+// alert("Hello World!"); // we are using node js, not browser alert only support in browsers
+
+// Code Readability
+
+console.log(3 + 3); console.log("Raashid"); // Do not prefer to write this type of codes
+
+console.log(3
+    +
+    3); // Do not prefer to write this type of codes, code readability should be high..
+
+// Datatypes
+
+// Primitive Datatypes
+let name = "Raashid";   //String
+let age = 20;           //Number
+let isLoggedIn = true;  //Boolean
+
+// number => 2 to power 53/52
+// bignit => big number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => variables declared but not assigned the value
+// symbol => unique
+
+//Non-Primitive Datatypes
+//Objects
+console.log(typeof null);       //output: object
+console.log(typeof undefined);  //output: undefined
