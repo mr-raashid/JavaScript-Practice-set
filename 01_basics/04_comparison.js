e1abfd479e31c296fe4895d1dbedcddab67cb614
mr-raashid <151937805+mr-raashid@users.noreply.github.com>
2024-02-12 14:28:01 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+//Basics
+
+// console.log(2 > 1);     // output: true
+// console.log(2 >= 1);    // output: true
+// console.log(2 < 1);     // output: false
+// console.log(2 <= 1);    // output: false
+// console.log(2 == 1);    // output: false
+// console.log(2 != 1);    // output: true
+
+//Comparison on different datatypes:(Avoid this type of comparisons)
+// console.log("2" > 1);       // output: true
+// console.log("02" > 1);      // output: true
+
+// console.log(null > 0);      // output: false
+// console.log(null == 0);     // output: false
+// console.log(null >= 0);     // output: true
+// console.log(null < 0);      // output: false
+// console.log(null <= 0);     // output: true
+
+// === (Triple equals to check the value and also datatypes):
+console.log("2" === 2);     //output: false
