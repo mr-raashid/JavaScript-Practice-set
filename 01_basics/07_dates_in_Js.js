b15b5bef2ceb78cce3fcce720ec562fa3558df6b
mr-raashid <151937805+mr-raashid@users.noreply.github.com>
2024-02-20 14:27:12 +0000

    array

--- 01_basics/07_dates_in_Js.js
+++ 01_basics/07_dates_in_Js.js
@@ -16,6 +16,19 @@ const myCreatedDate2 = Date();
 // console.log(typeof myCreatedDate2);     // typeof: string
 
 const myTimestamps = Date.now()
-console.log(myTimestamps);
-console.log(myCreatedDate1.getTime());
-console.log(Math.floor(myTimestamps/1000));     // To convert milliseconds to seconds
+// console.log(myTimestamps);
+// console.log(myCreatedDate1.getTime());
+// console.log(Math.floor(myTimestamps/1000));     // To convert milliseconds to seconds
+
+const newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+// console.log(newDate.getTime());
+
+const newDate1 = new Date();
+console.log(newDate1.toLocaleString("default", {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+}))
---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,12 @@
+// Arrays
+
+const myArr = [1, 2, 3, 4, 5];
+const myBikes = ["pulsar150", "dominar250", "ns200"];
+console.log(`My dream dike is ${myBikes[2]}`);
+console.log(`My dream dike is ${myBikes[myBikes.length-1]}`);
+
+//Another way to declare an Array:
+const newArr = new Array(10, 20, 30, 40);
+console.log(newArr[1]);
+
+// Array methods:
