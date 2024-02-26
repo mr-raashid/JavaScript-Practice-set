a7fbae64261d146bd9d7db694e3fbd4d0ba916e5
mr-raashid <151937805+mr-raashid@users.noreply.github.com>
2024-02-26 17:35:46 +0000

    Functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -38,8 +38,40 @@ console.log(loginMsg("Rash"));
 
 // Rest Operator & Spread Operator:
 
-function calculateCartPrice(...num1){
+function calculateCartPrice (...num1) {
     return num1;
 }
 
 console.log(calculateCartPrice(100, 200, 300));
+
+// We can pass an object to a function:
+
+const user = {
+    userName: "Raashid",
+    id: 1245
+}
+
+function handleObject (anyobject) {
+    return `id: ${anyobject.id} name: ${anyobject.userName}`
+}
+
+// console.log(handleObject(user));
+
+// Another way:
+function handleObject1 (anyobject1) {
+    return `id: ${anyobject1.id} name: ${anyobject1.userName}`
+}
+console.log(handleObject1({
+    userName: "Raiyan",
+    id: 1254
+}));
+
+// We can pass an Array to a function:
+
+const myArray = [ 100, 300, 800, 1200]
+
+function handleArray (getArray) {
+    return getArray[2];
+}
+// console.log(handleArray(myArray));
+console.log(handleArray([100, 200, 300, 400]));
