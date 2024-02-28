b0a75bb3702e5449fab65f2d3b2746bc79cbc536
mr-raashid <151937805+mr-raashid@users.noreply.github.com>
2024-02-28 14:32:25 +0000

    For-Each Loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,22 @@
+const coding = ["js", "py", "rb", "cpp", "java"];
+
+// For-Each loop with regular function:
+coding.forEach( function (item) {
+    // console.log(item);
+})
+
+// For-Each loop with arrow function:
+coding.forEach( (value) => {
+    // console.log(value);
+})
+
+// Passing function to the For-Each loop as a parameter:
+function printMe (item) {
+    // console.log(item);
+}
+coding.forEach(printMe);
+
+// For-Each has many parameters to pass into it:
+coding.forEach( (item, index, arr) => {
+    console.log(`item of index ${index} is: ${item} and the entire array is`, "+", arr );
+})
